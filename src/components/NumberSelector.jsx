7d95f3821d6f1ee5cd197418730831a48768d37d
Tanay Desai <tanaydesai26@gmail.com>
2021-02-01 01:59:47 +0530

    minor fix

--- src/components/NumberSelector.jsx
+++ src/components/NumberSelector.jsx
@@ -1,36 +1,54 @@
-import React,{useContext} from 'react'
-import {AppContext} from '../App'
-import './numberSelector.css'
-import ReactTooltip from 'react-tooltip';
+import React, { useContext } from "react";
+import { AppContext } from "../App";
+import { useHistory } from "react-router-dom";
+import "./numberSelector.css";
+import ReactTooltip from "react-tooltip";
 
 function NumberSelector(props) {
-    const {value,setValue,selected, setHighlight}=useContext(AppContext)
-    const onclick1=()=>{
-        if(selected[0]>=0){
-            var temp=JSON.parse(JSON.stringify(value))
-            // console.log(props.val.type.name)
-            if(props.val>=0 && props.val<10){
-                temp[selected[0]][selected[1]]=props.val.toString()
-            }else if(props.val.props.tip==="Delete"){
-                temp[selected[0]][selected[1]]=null
-            }else{ //add the methods to go back and forward here
-                // temp[selected[0]][selected[1]]=null
-            }
-            setValue(temp)
-            setHighlight(null)
-        }else{
-            setHighlight(props.val.toString())
-        }
+  const { value, setValue, selected, setHighlight, initialValue, timerControls, setDisplayError } = useContext(AppContext);
+  const history = useHistory();
+  const onclick1 = () => {
+    if (selected[0] >= 0) {
+      var temp = JSON.parse(JSON.stringify(value));
+      // console.log(props.val.type.name)
+      if (props.val >= 0 && props.val < 10) {
+        temp[selected[0]][selected[1]] = props.val.toString();
+      } else if (props.val.props.tip === "Delete") {
+        temp[selected[0]][selected[1]] = null;
+      }
+      //add the methods to go back and forward here
+      setValue(temp);
+      setHighlight(null);
+    } else {
+      setHighlight(props.val.toString());
     }
-    const dataTip=(props.val>=0 && props.val<10) ? null : props.val.props.tip
-    return (
-        <div className="numberSelector" onClick={onclick1}>
-            <div className="text" data-tip={dataTip}>
-                <ReactTooltip effect="solid" delayShow={500} border={true} borderColor=" rgb(56, 125, 204)"/>
-                {props.val}
-            </div>
-        </div>
-    )
+    if (!(props.val >= 0 && props.val < 10)) {
+      if (props.val.props.tip === "Reset") {
+        if (initialValue.length > 0) {
+          setValue(initialValue);
+          timerControls.current.reset();
+          timerControls.current.start();
+        }
+      } else if (props.val.props.tip === "Home") {
+        history.push("/");
+      } else if (props.val.props.tip === "Check") {
+        setDisplayError(true);
+        setTimeout(() => {
+          setDisplayError(false);
+        }, 2000);
+      } else {
+      }
+    }
+  };
+  const dataTip = props.val >= 0 && props.val < 10 ? null : props.val.props.tip;
+  return (
+    <div className="numberSelector" onClick={onclick1}>
+      <div className="text" data-tip={dataTip}>
+        <ReactTooltip effect="solid" delayShow={500} border={true} borderColor=" rgb(56, 125, 204)" />
+        {props.val}
+      </div>
+    </div>
+  );
 }
 
-export default NumberSelector
+export default NumberSelector;
